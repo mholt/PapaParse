d0f63a260e1cafa400700867dc73d22abcbab383
Matthew Holt <Matthew.Holt+git@gmail.com>
2014-12-20 19:50:18 -0700

    Papa gets a new look

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,12 +1,8 @@
-
-$(function()
+// Returns a random number between min and max (inclusive)
+function randomInt(min, max)
 {
-	
-});
-
-
-
-
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 // BEGIN GOOGLE ANALYTICS
---
+++ resources/js/lovers.js
@@ -0,0 +1,52 @@
+/**
+	INSTRUCTIONS
+
+	If you use Papa Parse, add your site/project/company to the list
+	below. Here is a description of the fields:
+
+		link: (optional) The URL to your web site
+
+		name: The name of the site/project/company
+
+		description: Say why Papa Parse is important to your
+		             application. The name is prepended to this
+		             description when rendered. Make sure that it
+		             reads as a fluid sentence when the name is
+		             concatenated with the description.
+
+		quote: (optional) A short testimonial of what you think
+		       of Papa Parse.
+
+	Note that the name and description are required. Please
+	use English and keep the length similar to existing entries;
+	entries that are too long or extremely short should be
+	edited before being accepted.
+
+	Thanks for being a part of Papa Parse!
+**/
+
+var peopleLovePapa = [
+	{
+		link: "https://smartystreets.com",
+		name: "SmartyStreets",
+		description: "verifies addresses, many of which are submitted in CSV files. Papa Parse can process files with over a million addresses in the browser.",
+		quote: "Because of Papa Parse, we rapidly built an awesome client-side list processing service."
+	},
+	{
+		link: "http://jannah.github.io/MetaReader",
+		name: "MetaReader",
+		description: "helps you see your data from a meta perspective before you start detailed analysis.",
+		quote: "Papa Parse made it very easy to load and ready user CSV files in the browser on the client side."
+	},
+	{
+		link: "https://github.com/JamesJansson/EpiML",
+		name: "EpiML",
+		description: "is an agent-based mathematical model for the web, still in its early stages of development.",
+		quote: "Papa makes it so easy to use CSV, which is good for scientists."
+	},
+	{
+		link: "https://wikipedia.org",
+		name: "Wikipedia",
+		description: "uses Papa Parse in VisualEditor to help article editors effortlessly build data tables from text files."
+	}
+];
