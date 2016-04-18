658dee34b3e75cab457a0fec87a1c23c41beaf25
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2016-04-18 17:24:39 +0200

    Fix indentation
    Spaces and tabs were mixed during copy&paste of external code.

--- papaparse.js
+++ papaparse.js
@@ -5,23 +5,23 @@
 */
 (function(root, factory)
 {
-  if (typeof define === 'function' && define.amd)
+	if (typeof define === 'function' && define.amd)
 	{
-    // AMD. Register as an anonymous module.
-    define([], factory);
-  }
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	}
 	else if (typeof module === 'object' && module.exports)
 	{
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory();
-  }
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	}
 	else
 	{
-    // Browser globals (root is window)
-    root.returnExports = factory();
-  }
+		// Browser globals (root is window)
+		root.returnExports = factory();
+	}
 }(this, function()
 {
 	'use strict';
