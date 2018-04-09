e85dad1f7ee9e092be3a05d8c6302c0ce03f666f
Gabe Gorelick <gabe@catalytic.com>
2018-04-09 18:13:41 -0500

    Replace connect with express
    Not a huge functional difference for how we're using it, but
    express is better maintained and much more active.

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,9 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
 var open = require('open');
 var path = require('path');
 var childProcess = require('child_process');
 
-var server = connect().use(serveStatic(path.join(__dirname, '/..'))).listen(8071, function () {
+var server = express().use(express.static(path.join(__dirname, '/..'))).listen(8071, function () {
 	if (process.argv.indexOf('--phantomjs') !== -1) {
 		childProcess.spawn('node_modules/.bin/mocha-phantomjs', ['http://localhost:8071/tests/tests.html'], {
 			stdio: 'inherit'
