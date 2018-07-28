cab82a57fe26ba345be15fc819ef3e30f66d335b
jaymeans <monkeydzeke@protonmail.com>
2018-07-28 15:57:30 -0700

    adding reference to source for escapeRegExp

--- papaparse.js
+++ papaparse.js
@@ -1295,6 +1295,7 @@
 		}
 	}
 
+	/** https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions */
 	function escapeRegExp(string)
 	{
 		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
