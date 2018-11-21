46160eed95bf39fa8a6ed0ec7291fe5054827446
Sergi Almacellas Abellana <sergi@koolpi.com>
2018-11-21 09:45:08 +0100

    Remove Array.isArray() polyfill
    Fixes #598

--- papaparse.js
+++ papaparse.js
@@ -5,15 +5,6 @@ https://github.com/mholt/PapaParse
 License: MIT
 */
 
-// Polyfills
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray#Polyfill
-if (!Array.isArray)
-{
-	Array.isArray = function(arg) {
-		return Object.prototype.toString.call(arg) === '[object Array]';
-	};
-}
-
 (function(root, factory)
 {
 	/* globals define */
