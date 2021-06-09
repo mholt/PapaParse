9341f8e3142658002f86557e40b91f7a36417bfe
Jordan Mele <jordan.mele@outlook.com.au>
2021-06-09 15:43:51 +1000

    Added ES module browser tests

--- tests/test.js
+++ tests/test.js
@@ -6,15 +6,22 @@ var childProcess = require('child_process');
 
 var server = connect().use(serveStatic(path.join(__dirname, '/..'))).listen(8071, function() {
 	if (process.argv.indexOf('--mocha-headless-chrome') !== -1) {
-		childProcess.spawn('node_modules/.bin/mocha-headless-chrome', ['-f', 'http://localhost:8071/tests/tests.html'], {
+		console.log('Running UMD tests...');
+		childProcess.spawn('node_modules/.bin/mocha-headless-chrome', ['-f', 'http://localhost:8071/tests/tests.umd.html'], {
 			stdio: 'inherit'
-		}).on('exit', function(code) {
-			server.close();
-			process.exit(code);  // eslint-disable-line no-process-exit
+		}).on('exit', function() {
+			console.log('Running ESM tests...');
+			childProcess.spawn('node_modules/.bin/mocha-headless-chrome', ['-f', 'http://localhost:8071/tests/tests.esm.html'], {
+				stdio: 'inherit'
+			}).on('exit', function(code) {
+				server.close();
+				process.exit(code);  // eslint-disable-line no-process-exit
+			});
 		});
 
 	} else {
-		open('http://localhost:8071/tests/tests.html');
+		open('http://localhost:8071/tests/tests.umd.html');
+		open('http://localhost:8071/tests/tests.esm.html');
 		console.log('Serving tests...');
 	}
 });
