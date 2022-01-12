cf7e11b133a96774752d41c94f400e28da4333cd
Aleks Bezrodnov <aleksei.bezrodnov@zalando.de>
2022-01-12 18:11:30 +0100

    Improve row skipping performance (#911)

--- papaparse.js
+++ papaparse.js
@@ -1163,9 +1163,9 @@ License: MIT
 
 			if (_config.skipEmptyLines)
 			{
-				for (var i = 0; i < _results.data.length; i++)
-					if (testEmptyLine(_results.data[i]))
-						_results.data.splice(i--, 1);
+				_results.data = _results.data.filter(function(d) {
+					return !testEmptyLine(d);
+				});
 			}
 
 			if (needsHeaderRow())
