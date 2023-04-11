30bab45f3dd885b038d814c99ae5cdcf14640f64
John Sherwood <john.sherwood.was.taken@gmail.com>
2023-04-11 16:45:16 +1000

    Use for loop for header parsing to only interate over array elements (#987)

--- papaparse.js
+++ papaparse.js
@@ -1478,7 +1478,8 @@ License: MIT
 				var headerCount = {};
 				var duplicateHeaders = false;
 
-				for (var j in headers) {
+				// Using old-style 'for' loop to avoid prototype pollution that would be picked up with 'var j in headers'
+				for (var j = 0; j < headers.length; j++) {
 					var header = headers[j];
 					if (isFunction(config.transformHeader))
 						header = config.transformHeader(header, j);
